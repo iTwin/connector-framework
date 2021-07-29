9f92de6fee035a447c85b71ae60c255e3612b0ea
Zachary Zhu <Zachary.Zhu@bentley.com>
2021-07-29 10:48:22 -0400

    modifies version and bug fix

--- src/test/ConnectorTestUtils.ts
+++ src/test/ConnectorTestUtils.ts
@@ -15,7 +15,7 @@ import {
 import { IModel } from "@bentley/imodeljs-common";
 import { AuthorizedClientRequestContext, ITwinClientLoggerCategory } from "@bentley/itwin-client";
 import { ConnectorLoggerCategory } from "../ConnectorLoggerCategory";
-import { ConnectorJobDefArgs } from "../itwin-connector-framework";
+import { ConnectorJobDefArgs } from "../connector-framework";
 import { IModelBankArgs, IModelBankUtils } from "../IModelBankUtils";
 import { IModelHubUtils } from "../IModelHubUtils";
 import { HubUtility } from "./integration/HubUtility";
