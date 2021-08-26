864fb8364e9832caf0a80311398f5f31a196453e
Zachary Zhu <Zachary.Zhu@bentley.com>
2021-08-26 12:14:47 -0400

    bye underscores

--- src/ConnectorRunner.ts
+++ src/ConnectorRunner.ts
@@ -79,7 +79,7 @@ export class ConnectorRunner {
     if (!this._reqContext || !(this._reqContext instanceof AuthorizedClientRequestContext))
       throw new Error("AuthorizedClientRequestContext has not been loaded.");
     if (this._reqContext.accessToken.isExpired(5)) {
-      this._reqContext.accessToken = await this._getToken();
+      this._reqContext.accessToken = await this.getToken();
       Logger.logInfo(LoggerCategories.Framework, "AccessToken Refreshed");
     }
     return this._reqContext;
@@ -142,7 +142,7 @@ export class ConnectorRunner {
   public async run(connectorFile: string): Promise<BentleyStatus> {
     let runStatus = BentleyStatus.SUCCESS;
     try {
-      await this._run(connectorFile);
+      await this.runUnsafe(connectorFile);
     } catch (err) {
       const msg = (err as any).message;
       Logger.logError(LoggerCategories.Framework, msg);
@@ -164,102 +164,102 @@ export class ConnectorRunner {
     return runStatus;
   }
 
-  private async _run(connectorFile: string) {
+  private async runUnsafe(connectorFile: string) {
     Logger.logInfo(LoggerCategories.Framework, "Connector Job has started");
 
     let reqContext: ClientRequestContext | AuthorizedClientRequestContext;
 
     // load
 
-    await this._loadConnector(connectorFile);
+    await this.loadConnector(connectorFile);
     Logger.logInfo(LoggerCategories.Framework, "ConnectorRunner.connector has been loaded.");
 
-    await this._loadReqContext();
+    await this.loadReqContext();
     Logger.logInfo(LoggerCategories.Framework, "ConnectorRunner.reqContext has been loaded.");
 
-    await this._loadDb();
+    await this.loadDb();
     Logger.logInfo(LoggerCategories.Framework, "ConnectorRunner.db has been loaded.");
 
-    await this._loadSynchronizer();
+    await this.loadSynchronizer();
     Logger.logInfo(LoggerCategories.Framework, "ConnectorRunner.connector.synchronizer has been loaded.");
 
-    this._initProgressMeter();
+    this.initProgressMeter();
 
     // source data
 
     Logger.logInfo(LoggerCategories.Framework, "connector.openSourceData started.");
-    await this._enterChannel(IModel.repositoryModelId);
+    await this.enterChannel(IModel.repositoryModelId);
 
     await this.connector.openSourceData(this.jobArgs.source);
     await this.connector.onOpenIModel();
 
-    await this._persistChanges(`Initialization`, ChangesType.Definition);
+    await this.persistChanges(`Initialization`, ChangesType.Definition);
     Logger.logInfo(LoggerCategories.Framework, "connector.openSourceData ended.");
 
     // domain schema
 
     Logger.logInfo(LoggerCategories.Framework, "connector.updateDomainSchema started");
-    await this._enterChannel(IModel.repositoryModelId);
+    await this.enterChannel(IModel.repositoryModelId);
 
     reqContext = await this.getReqContext();
     await this.connector.importDomainSchema(reqContext);
 
-    await this._persistChanges(`Domain Schema Update`, ChangesType.Schema);
+    await this.persistChanges(`Domain Schema Update`, ChangesType.Schema);
     Logger.logInfo(LoggerCategories.Framework, "connector.updateDomainSchema ended");
 
     // dynamic schema
 
     Logger.logInfo(LoggerCategories.Framework, "connector.importDynamicSchema started");
-    await this._enterChannel(IModel.repositoryModelId);
+    await this.enterChannel(IModel.repositoryModelId);
 
     reqContext = await this.getReqContext();
     await this.connector.importDynamicSchema(reqContext);
 
-    await this._persistChanges("Dynamic Schema Update", ChangesType.Schema);
+    await this.persistChanges("Dynamic Schema Update", ChangesType.Schema);
     Logger.logInfo(LoggerCategories.Framework, "connector.importDynamicSchema ended");
 
     // initialize job subject
 
     Logger.logInfo(LoggerCategories.Framework, "ConnectorRunner.updateJobSubject started");
-    await this._enterChannel(IModel.repositoryModelId);
+    await this.enterChannel(IModel.repositoryModelId);
 
-    const jobSubject = this._updateJobSubject();
+    const jobSubject = this.updateJobSubject();
 
-    await this._persistChanges(`Job Subject Update`, ChangesType.GlobalProperties);
+    await this.persistChanges(`Job Subject Update`, ChangesType.GlobalProperties);
     Logger.logInfo(LoggerCategories.Framework, "ConnectorRunner.updateJobSubject ended.");
 
     // definitions changes
     
     Logger.logInfo(LoggerCategories.Framework, "connector.importDefinitions started");
-    await this._enterChannel(jobSubject.id);
+    await this.enterChannel(jobSubject.id);
 
     await this.connector.initializeJob();
     await this.connector.importDefinitions();
 
-    await this._persistChanges("Definitions Update", ChangesType.Regular);
+    await this.persistChanges("Definitions Update", ChangesType.Regular);
     Logger.logInfo(LoggerCategories.Framework, "connector.importDefinitions ended");
 
     // data changes
     
     Logger.logInfo(LoggerCategories.Framework, "connector.updateExistingData started");
-    await this._enterChannel(jobSubject.id);
+    await this.enterChannel(jobSubject.id);
 
     await this.connector.updateExistingData();
-    this._updateDeletedElements();
-    this._updateProjectExtent();
+    this.updateDeletedElements();
+    this.updateProjectExtent();
 
-    await this._persistChanges("Data Update", ChangesType.Regular);
+    await this.persistChanges("Data Update", ChangesType.Regular);
     Logger.logInfo(LoggerCategories.Framework, "connector.updateExistingData ended");
 
     Logger.logInfo(LoggerCategories.Framework, "Connector Job has completed");
   }
 
-  private _updateDeletedElements() {
+  private updateDeletedElements() {
     if (this.jobArgs.doDetectDeletedElements)
       this.connector.synchronizer.detectDeletedElements();
   }
 
-  private _updateProjectExtent() {
+  private updateProjectExtent() {
     const res = this.db.computeProjectExtents({
       reportExtentsWithOutliers: false,
       reportOutliers: false,
@@ -267,7 +267,7 @@ export class ConnectorRunner {
     this.db.updateProjectExtents(res.extents);
   }
 
-  private _updateJobSubject(): Subject {
+  private updateJobSubject(): Subject {
     const code = Subject.createCode(this.db, IModel.rootSubjectId, this.jobSubjectName);
     const existingSubjectId = this.db.elements.queryElementIdByCode(code);
 
@@ -305,37 +305,37 @@ export class ConnectorRunner {
     return subject;
   }
 
-  private _initProgressMeter() {}
+  private initProgressMeter() {}
 
-  private async _loadConnector(connectorFile: string) {
+  private async loadConnector(connectorFile: string) {
     const connectorClass = require(connectorFile).default;
     this._connector = await connectorClass.create();
   }
 
-  private async _loadReqContext() {
+  private async loadReqContext() {
     const activityId = Guid.createValue();
     const appId = this.connector.getApplicationId();
     const appVersion = this.connector.getApplicationVersion();
     if (this.jobArgs.dbType === "briefcase") {
-      const token = await this._getToken();
+      const token = await this.getToken();
       this._reqContext = new AuthorizedClientRequestContext(token, activityId, appId, appVersion);
     } else {
       this._reqContext = new ClientRequestContext();
     }
   }
 
-  private async _getToken() {
+  private async getToken() {
     let token: AccessToken;
     if (!this.hubArgs)
       throw new Error("ConnectorRunner._getToken: undefined hubArgs.");
     if (this.hubArgs.doInteractiveSignIn)
-      token = await this._getTokenInteractive();
+      token = await this.getTokenInteractive();
     else
-      token = await this._getTokenSilent();
+      token = await this.getTokenSilent();
     return token;
   }
 
-  private async _getTokenSilent() {
+  private async getTokenSilent() {
     let token: AccessToken;
     if (this.hubArgs && this.hubArgs.tokenCallbackUrl) {
       const response = await fetch(this.hubArgs.tokenCallbackUrl);
@@ -349,7 +349,7 @@ export class ConnectorRunner {
     return token;
   }
 
-  private async _getTokenInteractive() {
+  private async getTokenInteractive() {
 		const client = new ElectronAuthorizationBackend();
     await client.initialize(this.hubArgs.clientConfig);
     return new Promise<AccessToken>((resolve, reject) => {
@@ -363,19 +363,19 @@ export class ConnectorRunner {
     });
   }
 
-  private async _loadDb() {
+  private async loadDb() {
     if (this.jobArgs.dbType === "briefcase") {
-      await this._loadBriefcaseDb();
+      await this.loadBriefcaseDb();
     } else if (this.jobArgs.dbType === "standalone") {
-      this._loadStandaloneDb();
+      this.loadStandaloneDb();
     } else if (this.jobArgs.dbType === "snapshot") {
-      this._loadSnapshotDb();
+      this.loadSnapshotDb();
     } else {
       throw new Error("Invalid JobArgs.dbType");
     }
   }
 
-  private async _loadSnapshotDb() {
+  private async loadSnapshotDb() {
     const cname = this.connector.getConnectorName();
     const fname = `${cname}.bim`;
     const fpath = path.join(this.jobArgs.stagingDir, fname);
@@ -384,7 +384,7 @@ export class ConnectorRunner {
     this._db = SnapshotDb.createEmpty(fpath, { rootSubject: { name: cname } });
   }
 
-  private async _loadStandaloneDb() {
+  private async loadStandaloneDb() {
     const cname = this.connector.getConnectorName(); 
     const fname = `${cname}.bim`;
     const fpath = path.join(this.jobArgs.stagingDir, fname);
@@ -394,7 +394,7 @@ export class ConnectorRunner {
       this._db = StandaloneDb.createEmpty(fpath, { rootSubject: { name: cname } });
   }
 
-  private async _loadBriefcaseDb() {
+  private async loadBriefcaseDb() {
 
     let bcFile: string | undefined = undefined;
     if (this.hubArgs.briefcaseFile) {
@@ -430,13 +430,13 @@ export class ConnectorRunner {
     (this._db as BriefcaseDb).concurrencyControl.startBulkMode();
   }
 
-  private async _loadSynchronizer() {
+  private async loadSynchronizer() {
     const reqContext = await this.getReqContext();
     const synchronizer = new Synchronizer(this.db, false, reqContext as AuthorizedClientRequestContext);
     this.connector.synchronizer = synchronizer;
   }
 
-  private async _persistChanges(changeDesc: string, ctype: ChangesType) {
+  private async persistChanges(changeDesc: string, ctype: ChangesType) {
     const { revisionHeader } = this.jobArgs;
     const comment = `${revisionHeader} - ${changeDesc}`;
     if (this.db.isBriefcaseDb()) {
@@ -451,7 +451,7 @@ export class ConnectorRunner {
     }
   }
 
-  private async _enterChannel(rootId: Id64String) {
+  private async enterChannel(rootId: Id64String) {
     if (!this.db.isBriefcaseDb())
       return;
 
