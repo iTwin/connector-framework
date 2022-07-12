4ec757f25599645126fb7cee61d9392c8d0d30a0
Jackson <107633044+jackson-at-bentley@users.noreply.github.com>
2022-07-12 15:00:24 -0700

    Restore the runner from `main`

--- src/ConnectorRunner.ts
+++ src/ConnectorRunner.ts
@@ -417,7 +417,7 @@ export class ConnectorRunner {
 
   private async getToken() {
     let token: string;
-    if (["standalone", "snapshot"].includes(this._jobArgs.dbType))
+    if (this._jobArgs.dbType === "snapshot")
       return "notoken";
 
     if (this.hubArgs.doInteractiveSignIn)
