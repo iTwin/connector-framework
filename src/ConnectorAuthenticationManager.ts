5d3846c739a5adee61333a41af8a77d3e0d49716
Jason.Chickneas <Jason.Chickneas@bentley.com>
2024-05-16 15:02:59 -0700

    ef: add comments to fix documentation build for new file and class.

--- src/ConnectorAuthenticationManager.ts
+++ src/ConnectorAuthenticationManager.ts
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 import { Logger, type AccessToken } from "@itwin/core-bentley";
 import type { AuthorizationClient } from "@itwin/core-common";
 import { LoggerCategories } from "./LoggerCategory";
