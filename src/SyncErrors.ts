fb58a5d8e840a778a1e04c226210885dd76d0b7c
Jason.Chickneas <Jason.Chickneas@bentley.com>
2024-08-28 10:49:40 -0700

    wip: add comments correct getCategory for reg. array vs. key value pair

--- src/SyncErrors.ts
+++ src/SyncErrors.ts
@@ -1,7 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 import * as fs from "fs";
 import { Logger } from "@itwin/core-bentley";
 import path = require("path");
 
+/**
+ * @description: The SyncError interface represents the structure of a single error in the Syncerr.json file
+ */
 export interface SyncError {
   system: string;
   phase: string;
@@ -12,11 +19,17 @@ export interface SyncError {
   canUserFix: boolean;
 }
 
+/**
+ * @description: The ErrorReport interface represents the structure of the Syncerr.json file
+*/
 export interface ErrorReport {
   version?: string;
   errors: SyncError[];
 }
 
+/**
+ * @description: The FatalErrorJSON interface represents the structure of the fatal-errors.json file
+ */
 export interface FatalErrorJSON {
   systems?: Object[];
   categories?: Object[];
@@ -24,6 +37,9 @@ export interface FatalErrorJSON {
   errors: FatalError[];
 }
 
+/**
+ * @description: The FatalErrorProps interface represents the properties of a fatal error
+ */
 interface FatalErrorProps {
   description: string;
   categoryId: string;
@@ -31,6 +47,9 @@ interface FatalErrorProps {
   canUserFix: boolean;
 }
 
+/**
+ * @description: The FatalError class represents a fatal error
+ */
 export class FatalError implements FatalErrorProps {
   constructor(properties: FatalErrorProps) {
     this.description = properties.description;
@@ -44,22 +63,37 @@ export class FatalError implements FatalErrorProps {
   public canUserFix: boolean;
 
 }
+
+/**
+ * @description: The FatalErrors class reads the fatal-errors.json file and provides methods to access the fatal errors
+ */
 export class FatalErrors {
   private _localPath: string;
   private _jsonObject: FatalErrorJSON| undefined;
   private _categories?: Object[];
   private _kbArticleLinks?: Object[];
   private _errors?: FatalError[];
+  /**
+   * @description: Constructor for the FatalErrors class
+   */
   constructor() {
     const directoryPath = this.getDirectoryPath();
     this._localPath = `${directoryPath}\\fatal-errors.json`;
   }
+
+  /**
+   * @description: The directory path to the assets folder
+   * @returns a string containing the path to the assets folder
+   */
   public getDirectoryPath(): string {
     const currentWorkingDirectory = process.cwd();
     const directoryPath = path.join(currentWorkingDirectory, "assets");
     return directoryPath;
   }
 
+  /**
+   * @description: Reads the fatal-errors.json file and populates the jsonObject property
+   */
   public read(): void {
     if (this._jsonObject)
       return;
@@ -72,6 +106,12 @@ export class FatalErrors {
     const data = fs.readFileSync(this._localPath, "utf8");
     this._jsonObject = JSON.parse(data);
   }
+
+  /**
+   * @description: Returns a sync error object populated with the fatal error matching the given errorKey
+   * @param errorKey index into the errors array
+   * @returns a sync error object populated with the fatal error matching the given errorKey or undefined if the errorKey does not exist
+   */
   public getError(errorKey: string): SyncError|undefined {
 
     if (this._jsonObject === undefined) {
@@ -93,6 +133,12 @@ export class FatalErrors {
 
     return syncErr;
   }
+
+  /**
+   * @description: Returns the url from an array of kbArticleLinks
+   * @param kbLinkId - a string containing a key to the kbArticleLinks array
+   * @returns a string containing the link(url) to the kb article if key exists in the kbArticleLinks array, otherwise undefined
+   */
   public getkbArticleLink(kbLinkId: string): string|undefined {
 
     if (this._jsonObject === undefined) {
@@ -110,21 +156,37 @@ export class FatalErrors {
     return kbArticleLink;
   }
 
-  public getCategory(categoryId: string): string|undefined {
-
+  /**
+   * @description: The category id to be checked
+   * @param categoryId
+   * @returns boolean indicating if the category exists or not
+   */
+  public categoryExists(categoryId: string): boolean {
     if (this._jsonObject === undefined) {
       Logger.logError("itwin-connector.Framework", `FatalErrors file has NOT been read!`);
-      return undefined;
+      return false;
     }
 
     if (this._jsonObject.categories === undefined) {
       Logger.logError("itwin-connector.Framework", `FatalErrors file has NO categories!`);
-      return undefined;
+      return false;
     }
 
-    const category: any = this._jsonObject.categories[categoryId as any];
-
-    return category;
+    return this._jsonObject.categories.indexOf(categoryId) >= 0;
   }
-}
 
+  /**
+   * @description: The category id to be checked
+   * @param categoryId
+   * @returns the validated category id if it exists or "other" (if 'other' exists) , otherwise undefined
+   */
+  public getCategory(categoryId: string): string|undefined {
+    if (this.categoryExists (categoryId))
+      return categoryId;
+    else if (this.categoryExists ("other"))
+      return "other";
+    else
+      return undefined;
+  }
+
+}
